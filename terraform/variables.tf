e478b04b6b57cc3414b72cc9967a29dfe0dd0d53
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-16 11:32:16 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region for deployment"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+}
